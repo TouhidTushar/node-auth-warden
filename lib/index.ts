242db95ff15edfd1ea19f1fdc998c3f3c177ae2a
TouhidTushar <tushartouhed@gmail.com>
2023-10-04 01:27:54 +0600

    fix(scaffolding): fix linting, formatting

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,3 @@
 export const hello = () => {
-  console.log("Hello 👋");
+  console.log('Hello 👋');
 };
--- src/tests/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from "vitest";
-
-describe("index", () => {
-  it("should pass CI", () => {
-    expect(true).toBe(true);
-  });
-});
